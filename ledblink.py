b32db0d1f9caa7500ec915e721f51e0125c740cc
Ryuya19 <s16025@tokyo.kosen-ac.jp>
2019-09-26 02:49:00 +0100

    LED点滅プログラム　s16025

---
+++ ledblink.py
@@ -0,0 +1,13 @@
+﻿import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.OUT)
+
+for i in range(10):
+    GPIO.output(2, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(2, GPIO.LOW)
+    time.sleep(0.5)
+
+GPIO.cleanup()
